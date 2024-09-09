23f191a4325eaf5ebc6dd051b710c1e971ea0423
Rahmatullo Qosimov <80538114+byStrange@users.noreply.github.com>
2024-09-09 13:33:00 +0500

    Update DateTimePickerCdnAsset.php

--- src/Asset/DateTimePickerCdnAsset.php
+++ src/Asset/DateTimePickerCdnAsset.php
@@ -23,14 +23,14 @@ final class DateTimePickerCdnAsset extends AssetBundle
     {
         parent::init();
 
-        $this->css = ['https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.38.0/css/tempusdominus-bootstrap-4.min.css'];
+        $this->css = ['https://cdn.jsdelivr.net/npm/tempusdominus-bootstrap-4@5.39.2/build/css/tempusdominus-bootstrap-4.min.css'];
         $this->cssOptions = [
             'crossorigin' => 'anonymous',
             'rel' => 'stylesheet',
         ];
         $this->js = [
             [
-                'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.38.0/js/tempusdominus-bootstrap-4.min.js',
+                'https://cdn.jsdelivr.net/npm/tempusdominus-bootstrap-4@5.39.2/build/js/tempusdominus-bootstrap-4.min.js',
                 'crossorigin' => 'anonymous',
             ],
         ];
